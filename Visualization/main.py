6cb42f7948e4c7d5dded362ce43f985bbb52b4ff
Aldrin Chrisostom <aldrinchrisostom15@gmail.com>
2023-02-17 17:25:32 +0530

    working

--- Visualization/main.py
+++ Visualization/main.py
@@ -18,29 +18,46 @@ except:
     print("Serial Port Error")
 
 
+
+def readSerial():
+
+    data = []
+
+    for i in range(tx):
+        arduinoDataString = ser.readline().decode()
+        formattedString = arduinoDataString.split(",")
+        formattedString[-1] = formattedString[-1].replace("$\r\n","")
+
+        formattedValues = [int(x) for x in formattedString[1:]]
+
+        if(re.search("\*\d\*",formattedString[0])): 
+            data.append(formattedValues) 
+        else:
+            print("Data Error")
+            data.append([0] * (rx ))
+        
+    return (data)
+
+
 def animate(dataList):
 
-    try:
-        arduinoData = np.array([[46, 45, 54, 45, 39, 40], [46, 45, random.randrange(90, 103), 45, 39, 40], [
-                               43, 42, 43, 45, 39, 40], [46, 45, 54, 45, 39, 40], [46, 45, 54, 45, 39, 40], [46, 45, 54, 45, 39, 40]])
-        dataList = arduinoData
-    except:
-        pass
 
-    x_range = np.arange(6)
-    y_range = np.arange(6)
+    dataList = np.array(readSerial())
+    print(dataList)
+
+
+    x_range = np.arange(len(dataList))
+    y_range = np.arange(len(dataList))
 
     x, y = np.meshgrid(x_range, y_range)
     ax.clear()
 
-    ax.plot_surface(x, y, dataList, rstride=1, cstride=1,
-                    linewidth=2, antialiased=False, shade=True)
+    ax.plot_surface(x, y, dataList)
 
     ax.set_zlim([0, 130])
     ax.set_title("Arduino Data")
 
 
-dataList = []
 
 
 fig = plt.figure()
@@ -49,34 +66,11 @@ ax = fig.add_subplot(projection='3d')
 
 time.sleep(2)
 
+dataList = []
 
 ani = animation.FuncAnimation(
     fig, animate, frames=100, fargs=(dataList), interval=100)
 
-print("Hello")
 plt.show()
 
 
-
-
-
-def readSerial():
-
-    data = []
-
-    for i in range(tx):
-        arduinoData_string = ser.readline().decode()
-        formattedString = arduinoData_string.split(",")
-        formattedString[-1] = formattedString[-1].replace("$\r\n","")
-
-        if(re.search("\*\d\*",formattedString[0])): 
-            data.append(formattedString[1:]) 
-        else:
-            print("Data Error")
-            data.append([1] * (rx - 1))
-        
-    return (data)
-
-
-if(ser):
-    print(readSerial())
